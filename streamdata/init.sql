dcb3fb0448fcdc30d9f74d3bccc0fd918d87f07f
James Sewell <james.sewell@gmail.com>
2024-02-09 06:44:10 +1300

    Add container

--- streamdata/init.sql
+++ streamdata/init.sql
@@ -1,4 +1,4 @@
 CREATE EXTENSION timescaledb;
-CREATE EXTENSION timescaledb-toolkit;
+CREATE EXTENSION timescaledb_toolkit;
 CREATE TABLE sensors (sensorid integer, ts timestamptz, value real);
 SELECT create_hypertable('sensors', 'ts', chunk_time_interval=> interval '1 day');
