ff9aa4e03cd58ecfe7b5de1b1144bf7b1d93223d
James Sewell <james.sewell@gmail.com>
2024-02-14 10:02:53 +1300

    none

--- streamdata/init.sql
+++ streamdata/init.sql
@@ -1,2 +1,3 @@
 CREATE TABLE sensors (sensorid text, ts timestamptz, value double precision);
-SELECT create_hypertable('sensors', 'ts', chunk_time_interval=> interval '1 day');
+SELECT create_hypertable('sensors', 'ts', chunk_time_interval=> interval '1 day', create_default_indexes=>false);
+CREATE INDEX ON sensors (sensorid, ts DESC);
